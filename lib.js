9148ad65f0cd8dc5068a7cf8cd214914359213fa
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-10-25 17:37:19 -0500

    Implemented the calculateTotals method in lib.js

--- lib.js
+++ lib.js
@@ -14,20 +14,23 @@ const cart =
 
 const listedPrice =
   listing =>
-    name =>
-      name === listing.name
-        ? listing.price
-        : 0
+  name =>
+  name === listing.name ?
+  listing.price :
+  0
 
 /**
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
-  listings =>
-    carts => {
-      // TODO
-    }
-
+  listings => carts => {
+    return carts.map(x => {
+      return {
+        customer: x.customer,
+        total: x.items.reduce((acc, x) => (typeof acc == "string") ? (listings.find(y => y.name === acc).price + listings.find(y => y.name === x).price) : (acc + listings.find(y => y.name === x).price))
+      }
+    })
+  }
 module.exports = {
   listing,
   cart,
